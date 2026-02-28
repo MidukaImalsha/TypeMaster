251c4811838da3723f075df4ea08161baa02271f
Miduka Imalsha <midukaonline@gmail.com>
2026-03-01 00:06:01 +0530

    Update database configuration to use environment variables

--- server/db/config.ts
+++ server/db/config.ts
@@ -2,11 +2,11 @@ import { createPool } from "mysql2/promise";
 require('dotenv').config();
 
 const pool = createPool({
-    host: "localhost",
-    user: process.env.DB_USER, // Replace with your MySQL username
-    password: process.env.DB_PASSWORD, // Replace with your MySQL password
-    database: "typing-game",
-    port: parseInt(process.env.MYSQL_PORT || "3306"), // Default MySQL port is 3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER, // Replace with your MySQL username
+    password: process.env.MYSQLPASSWORD, // Replace with your MySQL password
+    database: process.env.MYSQLDATABASE,
+    port: parseInt(process.env.MYSQLPORT || "3306"), // Default MySQL port is 3306
     waitForConnections: true,
     connectionLimit: 10, // Adjust as needed
     queueLimit: 0,
